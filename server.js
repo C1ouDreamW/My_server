61f089feffea43438c7897eafd79fd79727c28b4
C1ouD <cloud_aaa@163.com>
2025-12-03 22:17:51 +0800

    modified:   .gitignore 	modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -10,12 +10,12 @@ app.use(express.json()) // 允许服务器解析前端发来的JSON数据
 // 路由1：测试接口
 app.get('/api/test', (req, res) => {
   const mydata = {
-    message: "连接成功！",
-    time: new Date.toLocaleString(),
-    author: "C1ouD",
-  };
+    message: "连接成功",
+    time: new Date().toLocaleString(),
+    author: "C1ouD"
+  }
   res.json(mydata);
-});
+})
 
 // 路由2：接受前端提交的表单数据
 app.post('/api/contact', (req, res) => {
